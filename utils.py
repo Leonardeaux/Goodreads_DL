d55ea90d325e82cdabd846f7e7f3ed0deee3b5c6
leonardeaux <enzoleonardo@hotmail.fr>
2023-01-29 14:40:51 +0100

    First commit with new machine

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def predicted_test_data_to_result_csv(df_test, predicted_values):
+def predicted_test_data_to_result_csv(df_test, predicted_values, exp_name):
     review_ids = df_test.loc[:, 'review_id']
 
     max_predicted_values = predicted_values.argmax(axis=1) + 1
@@ -12,4 +12,4 @@ def predicted_test_data_to_result_csv(df_test, predicted_values):
 
     final_df['rating'] = final_df['rating'].apply(int)
 
-    final_df.to_csv('result.csv', index_label=False, index=False)
+    final_df.to_csv(f'res_files/{exp_name}.csv', index_label=False, index=False)
