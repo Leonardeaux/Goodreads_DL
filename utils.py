50730a9f12a384aa07210b129cbe53096439d890
leonardeaux <enzoleonardo@hotmail.fr>
2023-02-23 18:51:10 +0100

    adding rnn and transformer notebook

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import tensorflow as tf
 
 
 def predicted_test_data_to_result_csv(df_test, predicted_values, exp_name):
@@ -13,3 +14,30 @@ def predicted_test_data_to_result_csv(df_test, predicted_values, exp_name):
     final_df['rating'] = final_df['rating'].apply(int)
 
     final_df.to_csv(f'res_files/{exp_name}.csv', index_label=False, index=False)
+
+
+def load_dataset(frac_ratio: float):
+    train_path = "data/base/goodreads_train.csv"
+
+    df = pd.read_csv(train_path, sep=",")
+
+    index = df[(df['rating'] == 0)].index
+    df.drop(index, inplace=True)
+    df.reset_index(inplace=True, drop=True)
+
+    x_train = df.sample(frac=frac_ratio)
+    x_val = df.drop(x_train.index)
+
+    y_train = x_train.pop('rating')
+    y_train = y_train - 1
+
+    y_val = x_val.pop('rating')
+    y_val = y_val - 1
+
+    raw_train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10, reshuffle_each_iteration=False)
+
+    raw_val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val)).shuffle(10, reshuffle_each_iteration=False)
+
+
+
+    return x_train, x_val, y_train, y_val
